702b4dd521ea5f78a3b8459df128c8b6668c42d6
Dmitriy Rutkovskiy <rutkovski.dp@gmail.com>
2020-06-20 17:39:35 +0300

    blocking notification and screen on

--- app/src/main/java/com/rutkovski/interval/screens/TrainingActivity.java
+++ app/src/main/java/com/rutkovski/interval/screens/TrainingActivity.java
@@ -1,33 +1,27 @@
 package com.rutkovski.interval.screens;
 
-import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.SoundPool;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.rutkovski.interval.R;
 
+import java.util.Locale;
+
 public class TrainingActivity extends AppCompatActivity {
+
     private TextView textViewTimer;
     private TextView textViewAttempt;
     private ConstraintLayout constraintLayout;
@@ -35,15 +29,14 @@ public class TrainingActivity extends AppCompatActivity {
     private Button buttonEndWork;
     private Button buttonEndRelax;
     private TrainingViewModel trainingViewModel;
-    private SoundPool soundPool;
-    private int soundIdEndTimer;
+    private int attemptAll;
 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                this.finish();
+                createDialog(0).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -54,13 +47,12 @@ public class TrainingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
-
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         trainingViewModel = ViewModelProviders.of(this).get(TrainingViewModel.class);
-
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra("attempt") && intent.hasExtra("timeRelax")) {
             trainingViewModel.createTimer(intent.getLongExtra("timeRelax", 60000L));
-            trainingViewModel.setAttemptAll(intent.getIntExtra("attempt", 5));
+            attemptAll = intent.getIntExtra("attempt", 5);
         } else {
             finish();
         }
@@ -77,34 +69,38 @@ public class TrainingActivity extends AppCompatActivity {
         textViewWork = findViewById(R.id.textViewWork);
         buttonEndWork = findViewById(R.id.buttonEndAttempt);
         buttonEndRelax = findViewById(R.id.buttonEndRelax);
-
-
-
-        textViewAttempt.setText(trainingViewModel.counterString());
+        textViewAttempt.setText(String.format(Locale.getDefault(), "%d/%d", trainingViewModel.getAttemptNow(), attemptAll));
         trainingViewModel.getCurrentTime().observe(this, new Observer<String>() {
             @Override
             public void onChanged(String string) {
                 textViewTimer.setText(string);
             }
         });
-        trainingViewModel.getIsRunning().observe(this, new Observer<Integer>() {
+
+
+        trainingViewModel.getIsRunning().observe(this, new Observer<Boolean>() {
             @Override
-            public void onChanged(Integer isRun) {
-                if (isRun == 1) {
+            public void onChanged(Boolean isRun) {
+                if (isRun) {
                     runTimer();
                 } else {
                     stopTimer();
-
                 }
             }
         });
-
     }
 
     public void endAttempt(View view) {
         trainingViewModel.countAddAttemptNow();
-        textViewAttempt.setText(trainingViewModel.counterString());
-        trainingViewModel.startTimer();
+        int attemptNow = trainingViewModel.getAttemptNow();
+        textViewAttempt.setText(String.format(Locale.getDefault(), "%d/%d", attemptNow, attemptAll));
+        if (attemptNow != attemptAll) {
+            trainingViewModel.startTimer();
+        } else {
+            createDialog(1).show();
+        }
+
+
     }
 
     public void endRelax(View view) {
@@ -128,4 +124,49 @@ public class TrainingActivity extends AppCompatActivity {
     }
 
 
+    private AlertDialog createDialog(int id) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        if (id == 1) {
+            builder.setTitle(R.string.dialog_end_title);
+            builder.setMessage(R.string.dialog_end_description);
+            builder.setPositiveButton(R.string.dialog_button_continue, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    trainingViewModel.startTimer();
+                    dialogInterface.cancel();
+                }
+            });
+            builder.setNegativeButton(R.string.dialog_button_complete, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+        } else {
+            builder.setTitle(R.string.dialog_exit_title);
+            builder.setMessage(R.string.dialog_exit_description);
+            builder.setPositiveButton(R.string.dialog_button_continue, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            builder.setNegativeButton(R.string.dialog_button_complete, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+        }
+
+
+        builder.setCancelable(false);
+        return builder.create();
+    }
+
+    @Override
+    public void onBackPressed() {
+        createDialog(0).show();
+        super.onBackPressed();
+    }
 }
